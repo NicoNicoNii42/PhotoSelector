6c97f2e86ff9b631fec4e0a68274dc44c6c1a479
Nico Mäding <nicomaeding1996@googlemail.com>
2026-04-09 11:05:37 +0900

    Added script to reset test dir

---
+++ PhotoSorterAvalonia/ResetTestDir.sh
@@ -0,0 +1,1 @@
+cd "/Users/niconiconii/Pictures/DCIMTest/100LEICA" && find good verygood sortedout -name "*.DNG" -type f -exec mv {} . \; 2>/dev/null || true && echo "Reset complete! All .DNG files moved back to top level."
